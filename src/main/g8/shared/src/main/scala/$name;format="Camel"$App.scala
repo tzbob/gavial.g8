6f4e8ca378f2e4ee29f2a952fcca235eb2f62c43
Bob Reynders <Bob.Reynders@cs.kuleuven.be>
2019-03-01 15:55:12 +0100

    format name in include

--- src/main/g8/shared/src/main/scala/$name;format="Camel"$App.scala
+++ src/main/g8/shared/src/main/scala/$name;format="Camel"$App.scala
@@ -5,7 +5,7 @@ object $name;format="Camel"$App extends GavialApp {
   val port = 8080
   val host = "localhost"
 
-  val headExtensions = List(script(src := s"$name$-fastopt.js"))
+  val headExtensions = List(script(src := s"$name;format="lower"$-fastopt.js"))
 
   val ui = ClientDBehavior.constant {
     div(h1("Hello $name$!"),
