c8245410f07802e02b77176b9e806829ce21a48a
Bob Reynders <Bob.Reynders@cs.kuleuven.be>
2019-03-02 00:51:58 +0100

    refer to bundle

--- src/main/g8/shared/src/main/scala/$name;format="Camel"$App.scala
+++ src/main/g8/shared/src/main/scala/$name;format="Camel"$App.scala
@@ -5,7 +5,7 @@ object $name;format="Camel"$App extends GavialApp {
   val port = 8080
   val host = "localhost"
 
-  val headExtensions = List(script(src := s"$name;format="norm"$-fastopt.js"))
+  val headExtensions = List(script(src := s"$name;format="norm"$-fastopt-bundle.js"))
 
   val ui = ClientDBehavior.constant {
     div(h1("Hello $name$!"),
